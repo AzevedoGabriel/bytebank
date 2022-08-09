60b01681d1a9cdfbeb605d578090d55b7e7fc67e
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-08-09 14:10:39 -0300

    projeto angular

---
+++ src/app/nova-transfer/nova-transfer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'nova-transferencia',
+    templateUrl: './nova-transfer.component.html',
+    styleUrls: ['./nova-transfer.component.scss']
+})
+export class NovaTransferComponent{
+
+}
